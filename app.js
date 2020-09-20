54b574372bc87b4d006ff26e8259f9af0f10157b
Saral Shrivastava <shrivastavaa278@gmail.com>
2020-09-20 13:38:09 +0530

    deploy weather app

---
+++ app.js
@@ -0,0 +1,337 @@
+const countryListAlpha2 = {
+    "AF": "Afghanistan",
+    "AL": "Albania",
+    "DZ": "Algeria",
+    "AS": "American Samoa",
+    "AD": "Andorra",
+    "AO": "Angola",
+    "AI": "Anguilla",
+    "AQ": "Antarctica",
+    "AG": "Antigua and Barbuda",
+    "AR": "Argentina",
+    "AM": "Armenia",
+    "AW": "Aruba",
+    "AU": "Australia",
+    "AT": "Austria",
+    "AZ": "Azerbaijan",
+    "BS": "Bahamas (the)",
+    "BH": "Bahrain",
+    "BD": "Bangladesh",
+    "BB": "Barbados",
+    "BY": "Belarus",
+    "BE": "Belgium",
+    "BZ": "Belize",
+    "BJ": "Benin",
+    "BM": "Bermuda",
+    "BT": "Bhutan",
+    "BO": "Bolivia (Plurinational State of)",
+    "BQ": "Bonaire, Sint Eustatius and Saba",
+    "BA": "Bosnia and Herzegovina",
+    "BW": "Botswana",
+    "BV": "Bouvet Island",
+    "BR": "Brazil",
+    "IO": "British Indian Ocean Territory (the)",
+    "BN": "Brunei Darussalam",
+    "BG": "Bulgaria",
+    "BF": "Burkina Faso",
+    "BI": "Burundi",
+    "CV": "Cabo Verde",
+    "KH": "Cambodia",
+    "CM": "Cameroon",
+    "CA": "Canada",
+    "KY": "Cayman Islands (the)",
+    "CF": "Central African Republic (the)",
+    "TD": "Chad",
+    "CL": "Chile",
+    "CN": "China",
+    "CX": "Christmas Island",
+    "CC": "Cocos (Keeling) Islands (the)",
+    "CO": "Colombia",
+    "KM": "Comoros (the)",
+    "CD": "Congo (the Democratic Republic of the)",
+    "CG": "Congo (the)",
+    "CK": "Cook Islands (the)",
+    "CR": "Costa Rica",
+    "HR": "Croatia",
+    "CU": "Cuba",
+    "CW": "Curaçao",
+    "CY": "Cyprus",
+    "CZ": "Czechia",
+    "CI": "Côte d'Ivoire",
+    "DK": "Denmark",
+    "DJ": "Djibouti",
+    "DM": "Dominica",
+    "DO": "Dominican Republic (the)",
+    "EC": "Ecuador",
+    "EG": "Egypt",
+    "SV": "El Salvador",
+    "GQ": "Equatorial Guinea",
+    "ER": "Eritrea",
+    "EE": "Estonia",
+    "SZ": "Eswatini",
+    "ET": "Ethiopia",
+    "FK": "Falkland Islands (the) [Malvinas]",
+    "FO": "Faroe Islands (the)",
+    "FJ": "Fiji",
+    "FI": "Finland",
+    "FR": "France",
+    "GF": "French Guiana",
+    "PF": "French Polynesia",
+    "TF": "French Southern Territories (the)",
+    "GA": "Gabon",
+    "GM": "Gambia (the)",
+    "GE": "Georgia",
+    "DE": "Germany",
+    "GH": "Ghana",
+    "GI": "Gibraltar",
+    "GR": "Greece",
+    "GL": "Greenland",
+    "GD": "Grenada",
+    "GP": "Guadeloupe",
+    "GU": "Guam",
+    "GT": "Guatemala",
+    "GG": "Guernsey",
+    "GN": "Guinea",
+    "GW": "Guinea-Bissau",
+    "GY": "Guyana",
+    "HT": "Haiti",
+    "HM": "Heard Island and McDonald Islands",
+    "VA": "Holy See (the)",
+    "HN": "Honduras",
+    "HK": "Hong Kong",
+    "HU": "Hungary",
+    "IS": "Iceland",
+    "IN": "India",
+    "ID": "Indonesia",
+    "IR": "Iran (Islamic Republic of)",
+    "IQ": "Iraq",
+    "IE": "Ireland",
+    "IM": "Isle of Man",
+    "IL": "Israel",
+    "IT": "Italy",
+    "JM": "Jamaica",
+    "JP": "Japan",
+    "JE": "Jersey",
+    "JO": "Jordan",
+    "KZ": "Kazakhstan",
+    "KE": "Kenya",
+    "KI": "Kiribati",
+    "KP": "Korea (the Democratic People's Republic of)",
+    "KR": "Korea (the Republic of)",
+    "KW": "Kuwait",
+    "KG": "Kyrgyzstan",
+    "LA": "Lao People's Democratic Republic (the)",
+    "LV": "Latvia",
+    "LB": "Lebanon",
+    "LS": "Lesotho",
+    "LR": "Liberia",
+    "LY": "Libya",
+    "LI": "Liechtenstein",
+    "LT": "Lithuania",
+    "LU": "Luxembourg",
+    "MO": "Macao",
+    "MG": "Madagascar",
+    "MW": "Malawi",
+    "MY": "Malaysia",
+    "MV": "Maldives",
+    "ML": "Mali",
+    "MT": "Malta",
+    "MH": "Marshall Islands (the)",
+    "MQ": "Martinique",
+    "MR": "Mauritania",
+    "MU": "Mauritius",
+    "YT": "Mayotte",
+    "MX": "Mexico",
+    "FM": "Micronesia (Federated States of)",
+    "MD": "Moldova (the Republic of)",
+    "MC": "Monaco",
+    "MN": "Mongolia",
+    "ME": "Montenegro",
+    "MS": "Montserrat",
+    "MA": "Morocco",
+    "MZ": "Mozambique",
+    "MM": "Myanmar",
+    "NA": "Namibia",
+    "NR": "Nauru",
+    "NP": "Nepal",
+    "NL": "Netherlands (the)",
+    "NC": "New Caledonia",
+    "NZ": "New Zealand",
+    "NI": "Nicaragua",
+    "NE": "Niger (the)",
+    "NG": "Nigeria",
+    "NU": "Niue",
+    "NF": "Norfolk Island",
+    "MP": "Northern Mariana Islands (the)",
+    "NO": "Norway",
+    "OM": "Oman",
+    "PK": "Pakistan",
+    "PW": "Palau",
+    "PS": "Palestine, State of",
+    "PA": "Panama",
+    "PG": "Papua New Guinea",
+    "PY": "Paraguay",
+    "PE": "Peru",
+    "PH": "Philippines (the)",
+    "PN": "Pitcairn",
+    "PL": "Poland",
+    "PT": "Portugal",
+    "PR": "Puerto Rico",
+    "QA": "Qatar",
+    "MK": "Republic of North Macedonia",
+    "RO": "Romania",
+    "RU": "Russian Federation (the)",
+    "RW": "Rwanda",
+    "RE": "Réunion",
+    "BL": "Saint Barthélemy",
+    "SH": "Saint Helena, Ascension and Tristan da Cunha",
+    "KN": "Saint Kitts and Nevis",
+    "LC": "Saint Lucia",
+    "MF": "Saint Martin (French part)",
+    "PM": "Saint Pierre and Miquelon",
+    "VC": "Saint Vincent and the Grenadines",
+    "WS": "Samoa",
+    "SM": "San Marino",
+    "ST": "Sao Tome and Principe",
+    "SA": "Saudi Arabia",
+    "SN": "Senegal",
+    "RS": "Serbia",
+    "SC": "Seychelles",
+    "SL": "Sierra Leone",
+    "SG": "Singapore",
+    "SX": "Sint Maarten (Dutch part)",
+    "SK": "Slovakia",
+    "SI": "Slovenia",
+    "SB": "Solomon Islands",
+    "SO": "Somalia",
+    "ZA": "South Africa",
+    "GS": "South Georgia and the South Sandwich Islands",
+    "SS": "South Sudan",
+    "ES": "Spain",
+    "LK": "Sri Lanka",
+    "SD": "Sudan (the)",
+    "SR": "Suriname",
+    "SJ": "Svalbard and Jan Mayen",
+    "SE": "Sweden",
+    "CH": "Switzerland",
+    "SY": "Syrian Arab Republic",
+    "TW": "Taiwan",
+    "TJ": "Tajikistan",
+    "TZ": "Tanzania, United Republic of",
+    "TH": "Thailand",
+    "TL": "Timor-Leste",
+    "TG": "Togo",
+    "TK": "Tokelau",
+    "TO": "Tonga",
+    "TT": "Trinidad and Tobago",
+    "TN": "Tunisia",
+    "TR": "Turkey",
+    "TM": "Turkmenistan",
+    "TC": "Turks and Caicos Islands (the)",
+    "TV": "Tuvalu",
+    "UG": "Uganda",
+    "UA": "Ukraine",
+    "AE": "United Arab Emirates (the)",
+    "GB": "United Kingdom of Great Britain and Northern Ireland (the)",
+    "UM": "United States Minor Outlying Islands (the)",
+    "US": "United States of America (the)",
+    "UY": "Uruguay",
+    "UZ": "Uzbekistan",
+    "VU": "Vanuatu",
+    "VE": "Venezuela (Bolivarian Republic of)",
+    "VN": "Viet Nam",
+    "VG": "Virgin Islands (British)",
+    "VI": "Virgin Islands (U.S.)",
+    "WF": "Wallis and Futuna",
+    "EH": "Western Sahara",
+    "YE": "Yemen",
+    "ZM": "Zambia",
+    "ZW": "Zimbabwe",
+    "AX": "Åland Islands"
+};
+
+const weatherCond = {
+	"01d": "CLEAR_DAY",
+	"01n": "CLEAR_NIGHT",
+	"02d": "PARTLY_CLOUDY_DAY",
+	"02n": "PARTLY_CLOUDY_NIGHT",
+	"03d": "CLOUDY",
+	"03n": "CLOUDY",
+	"04d": "CLOUDY",
+	"04n": "CLOUDY",
+	"09d": "SLEET",
+	"09n": "SLEET",
+	"10d": "RAIN",
+	"10n": "RAIN",
+	"11d": "WIND",
+	"11n": "WIND",
+	"13d": "SNOW",
+	"13n": "SNOW",
+	"50d": "FOG",
+	"50n": "FOG",
+
+};
+
+window.addEventListener('load', () => {
+
+	let long;
+	let lat;
+	let temperatureDescription = document.querySelector('.temperature-description');
+	let temperatureDegree = document.querySelector('.temperature-degree');
+
+	let locationTimezone = document.querySelector('.location-timezone');
+	let tempeartureSection = document.querySelector('.degree-section');
+	let temperatureSpan = document.querySelector('.degree-section span');
+
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(position => {
+			long = position.coords.longitude;
+			lat=position.coords.latitude;
+			
+			const proxy = 'https://cors-anywhere.herokuapp.com/'
+			const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=29140e7afe92378316222f389d6738b7`	
+			fetch(api)
+				.then(response => {
+					return response.json();
+				})
+				.then(data => {
+					console.log(data);
+					const {temp, temp_min, temp_max} = data.main;
+					const {description, icon} = data.weather[0];
+					const city = data.name;
+					const country = countryListAlpha2[data.sys.country]
+					//Set DOM elements from the API
+					temperatureDegree.textContent = ((temp - 273.15) * (9/5) + 32).toFixed(0); // K to F
+					temperatureDescription.textContent = description.toUpperCase();
+					locationTimezone.textContent = `${country}/${city}`;
+					//Set Icon
+					setIcons(icon, document.querySelector(".icon"));
+
+
+					//Change temp to Celsius/ Farenheit
+					tempeartureSection.addEventListener('click', ()=>{
+						if(temperatureSpan.textContent === "F"){
+							temperatureSpan.textContent = "C";
+							temperatureDegree.textContent = temp - 273.15;
+						} else {
+							temperatureSpan.textContent = "F";
+							temperatureDegree.textContent = ((temp - 273.15) * (9/5) + 32).toFixed(0);
+						}
+					})
+				})
+		});
+
+
+
+	}
+
+	function setIcons(icon, iconID){
+		const skycons = new Skycons({color: "white"});
+		const currentIcon = weatherCond[icon];
+		skycons.play();
+		return skycons.set(iconID, Skycons[currentIcon]);
+	}
+});
+
+//https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=29140e7afe92378316222f389d6738b7
+//https://api.openweathermap.org/data/2.5/weather?lat=23&lon=77&appid=29140e7afe92378316222f389d6738b7
